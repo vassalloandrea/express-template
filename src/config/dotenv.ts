05407c103aafd9190978b0e420abe82893cec719
Andrea Vassallo <andrea.vassallo.94@gmail.com>
2020-10-06 00:01:06 +0200

    Add the ability to deploy the application

--- src/config/dotenv.ts
+++ src/config/dotenv.ts
@@ -3,4 +3,6 @@ import dotenv from 'dotenv'
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const dotenvConfiguration = dotenv.config()
-if (dotenvConfiguration.error) throw new Error('Could not find .env file')
+if (process.env.NODE_ENV !== 'production' && dotenvConfiguration.error) {
+  throw new Error('Could not find .env file')
+}
--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,4 +1,4 @@
-const port: number = parseInt(process.env.PORT ?? '3000', 10)
+const port: number = parseInt(process.env.PORT || '3000', 10)
 
 export {
   port
