55e593e7dc2dfd98803e8fb1c0548c484ca9ec5f
Andrea Vassallo <andrea.vassallo.94@gmail.com>
2020-10-05 21:53:38 +0200

    Allow the application to run express server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("Welcome to express")
+})
+
+app.listen(3000, () => {
+  console.log('App is listening to port 3000')
+})
