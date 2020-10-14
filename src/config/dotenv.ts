01bcdbbaa6179126257a190b8767e58e5c96e592
Andrea Vassallo <andrea.vassallo.94@gmail.com>
2020-10-14 18:53:09 +0200

    Allow Github to run the CI
    The CI will run tests and linters.

--- src/config/dotenv.ts
+++ src/config/dotenv.ts
@@ -3,6 +3,6 @@ import dotenv from 'dotenv'
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const dotenvConfiguration = dotenv.config()
-if (process.env.NODE_ENV !== 'production' && dotenvConfiguration.error) {
+if (process.env.NODE_ENV === 'development' && dotenvConfiguration.error) {
   throw new Error('Could not find .env file')
 }
