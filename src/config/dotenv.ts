b65cf6e3256d8a3fd9930f5ead2bbfff44fec333
Gianluca <eliagianluca94@gmail.com>
2020-10-22 00:16:15 +0200

    Add nodemailer configuration

--- src/config/dotenv.ts
+++ src/config/dotenv.ts
@@ -9,4 +9,25 @@ const dbUsername: string = process.env.DB_USERNAME || ''
 const dbPassword: string = process.env.DB_PASSWORD || ''
 const dbName: string = process.env.DB_NAME || ''
 
-export { env, isDevelopment, appUrl, port, dbUsername, dbPassword, dbName }
+// NodeMailer
+const mailer = {
+  host: process.env.SMTP_HOST || '',
+  port: Number(process.env.SMTP_PORT) || 2525,
+  auth: {
+    user: process.env.SMTP_USER || '',
+    pass: process.env.SMTP_PASSWORD || '',
+  },
+}
+const fromEmail: string = process.env.FROM_EMAIL || ''
+
+export {
+  env,
+  isDevelopment,
+  appUrl,
+  port,
+  dbUsername,
+  dbPassword,
+  dbName,
+  mailer,
+  fromEmail,
+}
