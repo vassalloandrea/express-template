512d73096ce8451b8b08c3049a60f067b7e2a475
Andrea Vassallo <andrea.vassallo.94@gmail.com>
2020-10-24 00:19:33 +0200

    Add the user list resolver method test

---
+++ src/resolvers/user-resolver/users.test.ts
@@ -0,0 +1,57 @@
+import request from 'supertest'
+
+import app from '../../app'
+import { databaseConnection } from '../../config'
+import { userService } from '../../services'
+
+const query = `
+  {
+    users {
+      name
+    }
+  }
+`
+
+describe('User list', () => {
+  beforeAll(async () => {
+    await databaseConnection.create()
+  })
+
+  afterAll(async () => {
+    await databaseConnection.close()
+  })
+
+  beforeEach(async () => {
+    await databaseConnection.clear()
+  })
+
+  it('returns all the users if exist', async (done) => {
+    await userService.createUser({ name: 'Foo Bar' })
+    await userService.createUser({ name: 'Bar Foo' })
+
+    request(app)
+      .post('/graphql')
+      .send({ query })
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body.data.users).toEqual([
+          { name: 'Foo Bar' },
+          { name: 'Bar Foo' },
+        ])
+        done()
+      })
+  })
+
+  it('returns an empty array if there are no users', (done) => {
+    request(app)
+      .post('/graphql')
+      .send({ query })
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body.data.users).toHaveLength(0)
+        done()
+      })
+  })
+})
